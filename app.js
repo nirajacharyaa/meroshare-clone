bc42d9ed5b17c74c0f205caf7e1b5181580864c3
nirajacharyaa <mail.nirajacharya@gmail.com>
2022-04-29 07:58:33 +0545

    initial commit

---
+++ app.js
@@ -0,0 +1,11 @@
+const hamburger = document.querySelector("div.hamburger > span");
+const sidebar = document.querySelector(".sidebar");
+const logo = document.querySelector("div.logo > img");
+hamburger.addEventListener("click", () => {
+  sidebar.classList.toggle("isClosed");
+  if (sidebar.classList.contains("isClosed")) {
+    logo.setAttribute("src", "./assets/brand-dash-icon.png");
+  } else {
+    logo.setAttribute("src", "./assets/brand-dash.png");
+  }
+});
