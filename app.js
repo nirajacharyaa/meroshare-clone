0a9d334f72004ec9dff786d641d82f18825ef54d
nirajacharyaa <mail.nirajacharya@gmail.com>
2022-05-13 11:27:02 +0545

    added transition

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
 const hamburger = document.querySelector("div.hamburger > span");
 const sidebar = document.querySelector(".sidebar");
 const logo = document.querySelector("div.logo > img");
+
 hamburger.addEventListener("click", () => {
-  sidebar.classList.toggle("isClosed");
-  if (sidebar.classList.contains("isClosed")) {
-    logo.setAttribute("src", "./assets/brand-dash-icon.png");
-  } else {
-    logo.setAttribute("src", "./assets/brand-dash.png");
-  }
+    sidebar.classList.toggle("isClosed");
+    if (sidebar.classList.contains("isClosed")) {
+        logo.setAttribute("src", "./assets/brand-dash-icon.png");
+    } else {
+        logo.setAttribute("src", "./assets/brand-dash.png");
+    }
 });
